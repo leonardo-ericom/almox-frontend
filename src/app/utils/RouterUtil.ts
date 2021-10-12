a92a723cc516045b79de7b23c93f44205e04af93
Patrick-Ribeiro <patrickribeiro.rb@gmail.com>
2021-10-12 01:55:32 -0300

    modified:   src/app/utils/RouterUtil.ts 	modified:   src/assets/theme/theme-dark-blue.css

--- src/app/utils/RouterUtil.ts
+++ src/app/utils/RouterUtil.ts
@@ -1,9 +1,11 @@
-import { ActivatedRouteSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot } from "@angular/router";
 
 const rotaEstaEmModoVisualizacao = (activatedRoute: ActivatedRouteSnapshot) => {
-    const [{ path }, {}] = activatedRoute.url;
+    console.log(activatedRoute)
+    if (!activatedRoute) return false;
+
+    const [{ path }] = activatedRoute.url;
     return path && path === "visualizar";
 };
 
 export { rotaEstaEmModoVisualizacao };
-
