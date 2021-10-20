1637279482f2d4d3521211b4361a1a465e3e109d
Patrick-Ribeiro <patrickribeiro.rb@gmail.com>
2021-10-20 20:53:15 -0300

    Formulário de produto

--- src/app/modules/produto/produto-form/produto-form.component.ts
+++ src/app/modules/produto/produto-form/produto-form.component.ts
@@ -1,21 +1,36 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { Departamento } from './../../../model/departamento';
+import { Fabricante } from './../../../model/fabricante';
+import { Grupo } from './../../../model/grupo';
 import { Produto } from './../../../model/produto';
+import { CommonService } from './../../shared/common.service';
 
 @Component({
     selector: "produto-form",
     templateUrl: "./produto-form.component.html",
 })
 export class ProdutoFormComponent implements OnInit {
-    produto: Produto = {};
+    produto: Produto = { configuracaoEstoque: {} };
     editandoRegistroExistente: boolean;
     modoVisualizacao: boolean;
     @ViewChild("formulario") formulario: NgForm;
+    unidadesMedidas: any[];
+    fabricantes: Fabricante[];
+    grupos: Grupo[];
+    departamentos: Departamento[];
 
-    constructor() {}
+    constructor(private commonService: CommonService) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.commonService
+            .buscarEnumeradores()
+            .subscribe(
+                enumeradores =>
+                    (this.unidadesMedidas = enumeradores.unidadesMedidas)
+            );
+    }
 
     onSubmit(formulario: NgForm): void {}
 
